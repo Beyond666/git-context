71bb8a77fafa98dd5b69ebe6d7d37ddc7db3d240
Beyond666 <39616303+Beyond666@users.noreply.github.com>
2021-07-08 23:18:56 +0800

    Update GitTest.java

--- src/main/java/com/liang/GitTest.java
+++ src/main/java/com/liang/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot text");
         System.out.println("master text");
         System.out.println("push text");
+        System.out.println("pull text");
     }
 }
